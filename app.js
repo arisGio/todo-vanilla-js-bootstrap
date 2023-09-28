12a8e6a422f98797011e37dc17368632736e6f21
Aris Giotis <apjoti@gmail.com>
2023-09-28 22:18:59 +0300

    Submit task & save to local storage

--- app.js
+++ app.js
@@ -4,5 +4,23 @@ const totalTasks = document.querySelector("#total-tasks");
 const remainingTasks = document.querySelector("#remaining-tasks");
 const completedTasks = document.querySelector("#completed-tasks");
 const mainInput = document.querySelector("form input");
-console.log(todoForm);
-console.log(mainInput);
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = mainInput.value;
+  if (inputValue == "") {
+    return;
+  }
+
+  const task = {
+    id: new Date().getTime(),
+    name: mainInput.value,
+    isCompleted: false,
+  };
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
