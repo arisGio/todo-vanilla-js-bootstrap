114d092e4e540d10a040904b4d6812d2423baecc
Aris Giotis <apjoti@gmail.com>
2023-09-28 22:08:05 +0300

    Functionality - Grab DOM elements

---
+++ app.js
@@ -0,0 +1,8 @@
+const todoForm = document.querySelector("form");
+const todoList = document.querySelector(".todos");
+const totalTasks = document.querySelector("#total-tasks");
+const remainingTasks = document.querySelector("#remaining-tasks");
+const completedTasks = document.querySelector("#completed-tasks");
+const mainInput = document.querySelector("form input");
+console.log(todoForm);
+console.log(mainInput);
